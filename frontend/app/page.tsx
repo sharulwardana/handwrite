346a7503858c00c1fdccaeb7d9f271a928c9633e
sharulwardana <sharulwrdn10@gmail.com>
2026-03-05 15:29:46 +0700

    tes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5306,7 +5306,7 @@ export default function Home() {
 
                                 <AnimatePresence mode="wait">
                                   <motion.div
-                                    key={activePageIndex}
+                                    key="flipbook-container"
                                     initial={{ opacity: 0, scale: 0.98 }}
                                     animate={{ opacity: 1, scale: 1 }}
                                     exit={{ opacity: 0, scale: 0.98 }}
